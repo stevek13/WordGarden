c692c0252e7379009ce198fc25b6117da7349cb2
Steve Kite <skite1@hotmail.com>
2023-10-31 09:16:58 -0700

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Steve on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
